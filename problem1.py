f456815d8830344cd3c67f58046fb586e156a93c
JulioLu <lulixhulio@gmail.com>
2024-04-13 14:15:20 +0300

    test

--- problem1.py
+++ problem1.py
@@ -1,11 +1,16 @@
-test_graph=[[0,1,2,1],
-            [1,0,1,1],
-            [2,1,0,1],
-            [1,1,1,0]]
+test_graph =[[0,1,2,1],[1,0,1,1],[2,1,0,1],[1,1,1,0]]
 
-print(test_graph)
 
-def drive(s, t, test_graph, L):
-    if(test_graph[s][t] != 0):
+print(test_graph[0])
+print(test_graph[1])
+print(test_graph[2])
+print(test_graph[3])
+
+
+
+
+def f(s,t,test_graph,L):
+    if test_graph[s][t] !=0:
         
-        
+
+
