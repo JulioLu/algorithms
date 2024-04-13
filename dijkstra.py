1ef3c9b2b1e71ee1a418b5353b46a33cd4f383cc
JulioLu <lulixhulio@gmail.com>
2024-04-13 15:43:48 +0300

    add the distances

--- dijkstra.py
+++ dijkstra.py
@@ -1,10 +1,4 @@
 
-test_graph = {
-    'A': {'B': 4, 'C': 2},
-    'B': {'A': 4, 'C': 5, 'D': 10},
-    'C': {'A': 2, 'B': 5, 'D': 3},
-    'D': {'B': 10, 'C': 3}
-}
 
 def drive(s,t,test_graph,L):
     distance = 0
@@ -17,4 +11,6 @@ def drive(s,t,test_graph,L):
             return True
 
 def dijkstra(graph, start):
+    d = graph['A'].values()
+    
         
