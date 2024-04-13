70904b1603c3d70e60b2b89fc544cd68eca96a40
giorgosskl <sklavenitisgeorge21@gmail.com>
2024-04-13 14:26:21 +0300

    drive through towns function

--- problem1.py
+++ problem1.py
@@ -1,17 +1,17 @@
-test_graph =[[0,1,2,1],[1,0,1,1],[2,1,0,1],[1,1,1,0]]
+test_graph=[[0,1,2,1],
+            [1,0,1,1],
+            [2,1,0,1],
+            [1,1,1,0]]
+
+print(test_graph)
 
 
-
-
-
-def f(s,t,test_graph,L):
+def drive(s,t,test_graph,L):
     distance = 0
     if test_graph[s][t] !=0:
         distance = test_graph[s][t]
-        if distance >L:
-            print("To long distance")
+        if L < distance :
+            print("Your tank can't cover the distance between two cities")
             return False
         else:
             return True
-    
-    
