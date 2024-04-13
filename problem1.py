39c28afb20f8545a9efaec9a3f020c6bbae75710
JulioLu <lulixhulio@gmail.com>
2024-04-13 14:17:38 +0300

    make the case when the nodes are connected

--- problem1.py
+++ problem1.py
@@ -1,16 +1,15 @@
 test_graph =[[0,1,2,1],[1,0,1,1],[2,1,0,1],[1,1,1,0]]
 
 
-print(test_graph[0])
-print(test_graph[1])
-print(test_graph[2])
-print(test_graph[3])
-
 
 
 
 def f(s,t,test_graph,L):
+    distance = 0
     if test_graph[s][t] !=0:
+        distance = test_graph[s][t]
+        if distance >=L:
+            print("To long distance")
+            return
         
-
 
