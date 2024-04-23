0d8096aceaa9226894c5f0afa5ff26b310304087
giorgosskl <sklavenitisgeorge21@gmail.com>
2024-04-23 10:59:35 +0300

    bigger node

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from dijkstra import dijkstra
 from dijkstra import shortest_path
 
 graph = {
-    'A': {'B': 4, 'C': 2},
-    'B': {'A': 4, 'C': 5, 'D': 10},
-    'C': {'A': 2, 'B': 5, 'D': 3},
-    'D': {'B': 10, 'C': 3}
+    'A': {'B': 2, 'C': 1, 'D': 4},
+    'B': {'A': 2, 'C': 3, 'E': 5},
+    'C': {'A': 1, 'B': 3, 'D': 2},
+    'D': {'A': 4, 'C': 2, 'E': 1},
+    'E': {'B': 5, 'D': 1}
 }
+
 
 if __name__ == "__main__":
     print(graph)
     print(graph['A'].values())
 
     start_node = 'A'
-    end_node = 'D'
+    end_node = 'E'
 
     shortest_path = shortest_path(graph, start_node, end_node)
     if shortest_path is not None:
