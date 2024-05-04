97cb706acab0ae6109488cc4306be9412582c714
giorgosskl <sklavenitisgeorge21@gmail.com>
2024-05-04 16:58:28 +0300

    problem 3

--- problem3.py
+++ problem3.py
@@ -1,23 +1,19 @@
-def calculate_split_cost(string_length, indexes):
-    indexes.sort()  # Ensure indexes are sorted in ascending order
-    total_cost = string_length  # Cost of the initial split
-
-    for i in range(len(indexes)):
-        if i == 0:
-            substring_length = indexes[i]
-        else:
-            substring_length = indexes[i] - indexes[i - 1]
-
-        total_cost += substring_length
-
-    # Add the cost of the substring after the last index
-    total_cost += string_length - indexes[-1]
-
-    return total_cost
+import numpy as np
 
 
-if __name__ == "__main__":
-    string_length = 15
-    indexes = [2, 4, 11]
-    total_cost = calculate_split_cost(string_length, indexes)
-    print("Total cost:", total_cost)
+# n is string len, P is of size m where P[i] is the split pos that split string into [1,i] and [i+1,n] (1-based)
+def spliting_cost(P, n):
+    P = [0] + P + [n]  # make sure pos list contains both ends of string
+    m = len(P)
+    P = [0] + P  # both C and P are 1-base indexed for easy reading
+    C = np.full((m + 1, m + 1), np.inf)
+    for i in range(1, m + 1): C[i, i:i + 2] = 0  # any segment <= 2 does not need split so is zero cost
+    for s in range(2, m):  # s is split string len
+        for i in range(1, m - s + 1):
+            j = i + s
+            for k in range(i, j + 1):
+                C[i, j] = min(C[i, j], P[j] - P[i] + C[i, k] + C[k, j])
+    return C[1, m]
+
+
+print(spliting_cost([10, 18, 19], 20))
