2ab36f4548547afd5dbb39c2e83f87d351c8b931
giorgosskl <sklavenitisgeorge21@gmail.com>
2024-05-04 17:12:37 +0300

    function changes

--- problem3.py
+++ problem3.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-def spliting_cost(P, n):
-    P = [0] + P + [n]
-    m = len(P)
-    P = [0] + P
-    C = np.full((m + 1, m + 1), np.inf)
+def calculate_splitting_cost(prices, num_parts):
+    prices = [0] + prices + [num_parts]
+    m = len(prices)
+    prices = [0] + prices
+    costs = np.full((m + 1, m + 1), np.inf)
     for i in range(1, m + 1):
-        C[i, i:i + 2] = 0
+        costs[i, i:i + 2] = 0
 
     for s in range(2, m):
         for i in range(1, m - s + 1):
             j = i + s
             for k in range(i, j + 1):
-                C[i, j] = min(C[i, j], P[j] - P[i] + C[i, k] + C[k, j])
-    return C[1, m]
+                costs[i, j] = min(costs[i, j], prices[j] - prices[i] + costs[i, k] + costs[k, j])
+    return costs[1, m]
 
 
 string = "abcdabcdabcdabcdabcd"
 print("Length of string:", len(string))
-print("Minimum splitting cost:", spliting_cost([10, 18, 19], len(string)))
+print("Minimum splitting cost:", calculate_splitting_cost([10, 18, 19], len(string)))
